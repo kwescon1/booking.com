963d5f104d144cce21814aafe35f9aea8b57d049
Odame <kwesi.danquah@daaseng.com>
2023-06-23 15:54:52 +0100

    observe user class

---
+++ app/Observers/UserObserver.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserObserver
+{
+    /**
+     * Handle the User "creating" event.
+     */
+    public function creating(User $user): void
+    {
+        $user->password = Hash::make($user->password);
+
+    }
+}
