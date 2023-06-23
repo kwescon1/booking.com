beea8db3d5c052867f82cae2cde1e140df80b728
Odame <kwesi.danquah@daaseng.com>
2023-06-23 15:55:09 +0100

    validation rules for storing user

---
+++ app/Http/Requests/RegisterUserRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\Enums\Roles;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+use Illuminate\Validation\Rules\Password;
+
+class RegisterUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255'],
+            'password' => ['required', 'confirmed', Password::defaults()],
+            'role_id' => ['required', Rule::in(Roles::ROLE_OWNER, Roles::ROLE_USER)],
+        ];
+    }
+}
