10525ef305e0ced2557bead3249496d42c429e51
Odame <kwesi.danquah@daaseng.com>
2023-06-23 15:56:24 +0100

    register user method

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\RegisterUserRequest;
+use App\Models\User;
+
+class RegisterController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(RegisterUserRequest $request)
+    {
+        //
+        $data = $request->validated();
+
+        $user = User::create($data);
+
+        return response()->json([
+            'access_token' => $user->createToken('client')->plainText,
+        ]);
+
+    }
+}
